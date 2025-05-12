f03d8630321d8c152804e5c59fd51b811bfd06e3
Komediruzecki <komediruzecki.2015@gmail.com>
2025-05-12 22:51:34 +0000

    Add linear1 example

--- packages/app/src/flame/examples/index.ts
+++ packages/app/src/flame/examples/index.ts
@@ -1,9 +1,11 @@
 import { empty } from './empty'
 import { example1 } from './example1'
 import type { FlameFunction } from '../flameFunction'
+import { linear1 } from './linear1'
 
 export const examples = {
   example1,
+  linear1,
   empty,
 } satisfies Record<string, FlameFunction[]>
 export type ExampleID = keyof typeof examples
