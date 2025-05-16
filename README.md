d26ae85b8fa8b3eb6ce4ff54d0d78402477421d1
David Emanuel Luksic <deluksic@gmail.com>
2025-05-16 20:38:07 +0200

    Add .env defaults

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Chaos-Master CI](https://github.com/chaos-matters/chaos-master/actions/workflows/node.js.yml/badge.svg)](https://github.com/chaos-matters/chaos-master/actions/workflows/node.js.yml)
+
 # Chaos Master
 
 WebGPU based IFS Flame Generator for Artists and Explorers
---
+++ packages/app/src/defaults.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_POINT_COUNT = parseFloat(
+  import.meta.env.VITE_DEFAULT_POINT_COUNT,
+)
+export const DEFAULT_RESOLUTION = parseFloat(
+  import.meta.env.VITE_DEFAULT_RESOLUTION,
+)
+export const DEFAULT_RENDER_INTERVAL_MS = parseFloat(
+  import.meta.env.VITE_DEFAULT_RENDER_INTERVAL_MS,
+)
