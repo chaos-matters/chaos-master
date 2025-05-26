6ede94d38fe39df44e792b1397f790a8d62056cd
David Emanuel Luksic <deluksic@gmail.com>
2025-05-27 01:37:09 +0200

    Unify modal title bars and closing mechanism

---
+++ packages/app/src/components/Modal/ModalTitleBar.tsx
@@ -0,0 +1,42 @@
+import { Show } from 'solid-js'
+import { Cross } from '@/icons'
+import { useKeyboardShortcuts } from '@/utils/useKeyboardShortcuts'
+import { Button } from '../Button/Button'
+import ui from './ModalTitleBar.module.css'
+import type { ParentProps } from 'solid-js'
+
+type ModalTitleBarProps = {
+  onClose?: () => void
+}
+
+/**
+ * Modal title bar which has a close button (if `onClose` provided) for light dismiss.
+ * It also allows closing the modal using Esc key.
+ */
+export function ModalTitleBar(props: ParentProps<ModalTitleBarProps>) {
+  useKeyboardShortcuts({
+    Escape: () => {
+      if (props.onClose) {
+        props.onClose()
+        return true
+      }
+    },
+  })
+  return (
+    <h1 class={ui.title}>
+      <span>{props.children}</span>
+      <Show when={props.onClose} keyed>
+        {(onClose) => (
+          <Button
+            class={ui.closeButton}
+            onClick={() => {
+              onClose()
+            }}
+          >
+            <Cross width="1rem" />
+          </Button>
+        )}
+      </Show>
+    </h1>
+  )
+}
---
+++ packages/app/src/icons/index.ts
@@ -0,0 +1,8 @@
+import Cross from './cross.svg'
+import Minus from './minus.svg'
+import Plus from './plus.svg'
+import Redo from './redo.svg'
+import Undo from './undo.svg'
+
+// Re-exported for easier auto-importing in components
+export { Cross, Minus, Plus, Redo, Undo }
