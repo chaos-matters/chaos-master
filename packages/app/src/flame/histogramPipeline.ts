9baf9da32dff3d664e409f94693d1767c4ce8aa6
David Emanuel Luksic <deluksic@gmail.com>
2025-06-03 00:27:00 +0200

    Implement counts histogram

---
+++ packages/app/src/flame/histogramPipeline.ts
@@ -0,0 +1,83 @@
+import { tgpu } from 'typegpu'
+import { arrayOf, atomic, u32 } from 'typegpu/data'
+import { wgsl } from '@/utils/wgsl'
+import type { LayoutEntryToInput, TgpuRoot } from 'typegpu'
+
+export const HISTOGRAM_BIN_COUNT = 1024
+const GROUP_SIZE_X = 8
+const GROUP_SIZE_Y = 4
+
+const { ceil } = Math
+
+const bindGroupLayout = tgpu.bindGroupLayout({
+  accumulationTexture: {
+    texture: 'unfilterable-float',
+  },
+  histogram: {
+    storage: (length: number) => arrayOf(atomic(u32), length),
+    access: 'mutable',
+  },
+})
+
+export function createHistogramPipeline(
+  root: TgpuRoot,
+  textureSize: [number, number],
+  accumulationTexture: LayoutEntryToInput<
+    (typeof bindGroupLayout)['entries']['accumulationTexture']
+  >,
+  histogram: LayoutEntryToInput<
+    (typeof bindGroupLayout)['entries']['histogram']
+  >,
+) {
+  const { device } = root
+
+  const bindGroup = root.createBindGroup(bindGroupLayout, {
+    accumulationTexture,
+    histogram,
+  })
+
+  const moduleCode = wgsl/* wgsl */ `
+    ${{
+      ...bindGroupLayout.bound,
+    }}
+
+    @compute @workgroup_size(${GROUP_SIZE_X}, ${GROUP_SIZE_Y}, 1) fn run(
+      @builtin(global_invocation_id) global_invocation_id: vec3u
+    ) {
+      let dims = vec2i(textureDimensions(accumulationTexture));
+      let uv = vec2i(global_invocation_id.xy);
+      if (uv.x >= dims.x || uv.y >= dims.y) {
+        return;
+      }
+      let centralTexel = textureLoad(accumulationTexture, uv, 0);
+      let count = centralTexel.a;
+      let binCount = arrayLength(&histogram);
+      let bin = clamp(u32(count), 0, binCount);
+      atomicAdd(&histogram[bin], 1u);
+    }
+  `
+
+  const module = device.createShaderModule({
+    code: moduleCode,
+  })
+
+  const pipeline = device.createComputePipeline({
+    layout: device.createPipelineLayout({
+      bindGroupLayouts: [root.unwrap(bindGroupLayout)],
+    }),
+    compute: {
+      module: module,
+    },
+  })
+
+  return (pass: GPUComputePassEncoder) => {
+    const [width, height] = textureSize
+    pass.setPipeline(pipeline)
+    pass.setBindGroup(0, root.unwrap(bindGroup))
+    pass.dispatchWorkgroups(
+      ceil(width / GROUP_SIZE_X),
+      ceil(height / GROUP_SIZE_Y),
+      1,
+    )
+  }
+}
