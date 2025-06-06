2b6e0b5545bd40e42a14b7630dd640c4f5819313
David Emanuel Luksic <deluksic@gmail.com>
2025-06-07 00:29:10 +0200

    Implement pinch to zoom

--- packages/app/src/lib/WheelZoomCamera2D.tsx
+++ packages/app/src/lib/WheelZoomCamera2D.tsx
@@ -6,15 +6,18 @@ import {
   onCleanup,
 } from 'solid-js'
 import { vec2f } from 'typegpu/data'
-import { clamp } from 'typegpu/std'
+import { clamp, sub } from 'typegpu/std'
 import { vec2 } from 'wgpu-matrix'
 import { Camera2D } from '@/lib/Camera2D'
 import { useCamera } from '@/lib/CameraContext'
 import { useCanvas } from '@/lib/CanvasContext'
 import { createDragHandler } from '@/utils/createDragHandler'
+import { createPinchHandler } from '@/utils/createPinchHandler'
 import { eventToClip } from '@/utils/eventToClip'
 import type { ParentProps, Setter, Signal } from 'solid-js'
 import type { v2f } from 'typegpu/data'
+
+const SCROLL_SENSITIVITY = 0.001
 
 type WheelZoomCamera2DProps = {
   zoom: Signal<number>
@@ -37,6 +40,7 @@ export function createPosition(initPos: v2f): Signal<v2f> {
 
   return [position, setPosition]
 }
+
 export function createZoom(
   initZoom: number,
   zoomRange: [number, number],
@@ -66,7 +70,7 @@ export function WheelZoomCamera2D(props: ParentProps<WheelZoomCamera2DProps>) {
 
   let clipToWorld: (clip: v2f) => v2f | undefined
 
-  const pan = createDragHandler((initEvent) => {
+  const startPanning = createDragHandler((initEvent) => {
     const grabPosition = clipToWorld(eventToClip(initEvent, el()))
     if (!grabPosition) {
       return
@@ -82,6 +86,21 @@ export function WheelZoomCamera2D(props: ParentProps<WheelZoomCamera2DProps>) {
     }
   })
 
+  function zoomKeepPointInPlace(world: v2f, ratio: number) {
+    const oldZoom = zoom()
+    batch(() => {
+      const newZoom = setZoom(oldZoom * ratio)
+      // actual ratio can be different due to min/max zoom level clamping
+      const actualRatio = oldZoom / newZoom
+      setPosition(({ x, y }) =>
+        vec2f(
+          x + (world.x - x) * (1 - actualRatio),
+          y + (world.y - y) * (1 - actualRatio),
+        ),
+      )
+    })
+  }
+
   function onWheel(ev: WheelEvent) {
     ev.preventDefault()
     const clip = eventToClip(ev, el())
@@ -89,22 +108,37 @@ export function WheelZoomCamera2D(props: ParentProps<WheelZoomCamera2DProps>) {
     if (!world) {
       return
     }
-    const oldZoom = zoom()
-    batch(() => {
-      const newZoom = setZoom(oldZoom * (1 - ev.deltaY * 0.001))
-      const ratio = oldZoom / newZoom
-      setPosition(({ x, y }) =>
-        vec2f(x + (world.x - x) * (1 - ratio), y + (world.y - y) * (1 - ratio)),
-      )
-    })
+    zoomKeepPointInPlace(world, 1 - ev.deltaY * SCROLL_SENSITIVITY)
   }
+
+  const startPinch = createPinchHandler((initEvent) => {
+    const grabPosition = clipToWorld(eventToClip(initEvent.midpoint, el()))
+    if (!grabPosition) {
+      return
+    }
+    let prevDistance = initEvent.distance
+    return {
+      onPinchMove(event) {
+        const pinchRatio = event.distance / prevDistance
+        const world = clipToWorld(eventToClip(event.midpoint, el()))
+        if (!world) {
+          return
+        }
+        setPosition((prev) => sub(prev, sub(world, grabPosition)))
+        zoomKeepPointInPlace(world, pinchRatio)
+        prevDistance = event.distance
+      },
+    }
+  })
 
   createEffect(() => {
     const eventTarget = el()
-    eventTarget.addEventListener('pointerdown', pan)
+    eventTarget.addEventListener('pointerdown', startPanning)
+    eventTarget.addEventListener('touchmove', startPinch)
     eventTarget.addEventListener('wheel', onWheel, { passive: false })
     onCleanup(() => {
-      eventTarget.removeEventListener('pointerdown', pan)
+      eventTarget.removeEventListener('pointerdown', startPanning)
+      eventTarget.removeEventListener('touchmove', startPinch)
       eventTarget.removeEventListener('wheel', onWheel)
     })
   })
--- packages/app/src/utils/assertArray.ts
+++ packages/app/src/utils/assertArray.ts
@@ -12,6 +12,7 @@
 export function hasAtLeastOneElement<T>(array: T[]): array is [T, ...T[]] {
   return array.length > 0
 }
+
 /**
  * Type guard to make sure an array has exactly one element,
  * so it can be indexed into using `[0]` or unpacked like:
@@ -24,5 +25,20 @@ export function hasAtLeastOneElement<T>(array: T[]): array is [T, ...T[]] {
  * ```
  */
 export function hasExactlyOneElement<T>(array: T[]): array is [T] {
-  return array.length > 0
+  return array.length === 1
+}
+
+/**
+ * Type guard to make sure an array has exactly two elements,
+ * so it can be indexed into using `[0], [1]` or unpacked like:
+ * ```
+ * let array: number[]
+ * if (hasExactlyTwoElements(array)) {
+ *   const [first, second] = array
+ *   // first and second is number, not number | undefined
+ * }
+ * ```
+ */
+export function hasExactlyTwoElements<T>(array: T[]): array is [T, T] {
+  return array.length === 2
 }
