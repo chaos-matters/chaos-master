ad458f3f4d846aaa5a5a9e856b233a9f2b17a058
David Emanuel Luksic <deluksic@gmail.com>
2025-06-07 00:29:10 +0200

    Implement pinch to zoom

--- packages/app/src/utils/assertArray.ts
+++ packages/app/src/utils/assertArray.ts
@@ -12,6 +12,7 @@
 export function hasAtLeastOneElement<T>(array: T[]): array is [T, ...T[]] {
   return array.length > 0
 }
+
 /**
  * Type guard to make sure an array has exactly one element,
  * so it can be indexed into using `[0]` or unpacked like:
@@ -25,4 +26,19 @@ export function hasAtLeastOneElement<T>(array: T[]): array is [T, ...T[]] {
  */
 export function hasExactlyOneElement<T>(array: T[]): array is [T] {
   return array.length > 0
+}
+
+/**
+ * Type guard to make sure an array has exactly two elements,
+ * so it can be indexed into using `[0], [1]` or unpacked like:
+ * ```
+ * let array: number[]
+ * if (hasExactlyTwoElements(array)) {
+ *   const [first, second] = array
+ *   // first and second is number, not number | undefined
+ * }
+ * ```
+ */
+export function hasExactlyTwoElements<T>(array: T[]): array is [T, T] {
+  return array.length === 2
 }
