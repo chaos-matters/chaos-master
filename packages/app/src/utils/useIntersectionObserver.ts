17d2d0d3aefd4fdc79c82fb9970a74461c7cebab
David Emanuel Luksic <deluksic@gmail.com>
2026-03-15 21:45:55 +0100

    Limit number of running flames in var selector

--- packages/app/src/utils/useIntersectionObserver.ts
+++ packages/app/src/utils/useIntersectionObserver.ts
@@ -1,10 +1,12 @@
-import { createEffect, onCleanup } from 'solid-js'
+import { createEffect, createSignal, onCleanup } from 'solid-js'
 import type { Accessor } from 'solid-js'
 
 export function useIntersectionObserver(
   target: Accessor<HTMLElement | null | undefined>,
   onChange?: (isVisible: boolean) => void,
 ) {
+  const [intersection, setIntersection] =
+    createSignal<IntersectionObserverEntry>()
   createEffect(() => {
     const t = target()
     if (!t) {
@@ -12,16 +14,19 @@ export function useIntersectionObserver(
     }
     const observer = new IntersectionObserver(([entry]) => {
       if (entry === undefined || !t.isConnected) {
+        setIntersection(undefined)
         return
       }
       const isVisible = entry.isIntersecting
       if (onChange !== undefined) {
         onChange(isVisible)
       }
+      setIntersection(entry)
     })
     observer.observe(t)
     onCleanup(() => {
       observer.disconnect()
     })
   })
+  return intersection
 }
