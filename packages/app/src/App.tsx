5c43fa6005f07186341f8e3260675ee5a597ef0d
komediruzecki <komediruzecki.2015@gmail.com>
2026-05-25 21:57:07 +0200

    Fix lints, typecheck, reformat

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,104 +1,24 @@
-import { batch, createEffect, createMemo, createResource, createSignal, ErrorBoundary, For, onCleanup, onMount, Show, Suspense, } from 'solid-js'
-import { createStore } from 'solid-js/store'
-import { Dynamic } from 'solid-js/web'
-import { vec2f, vec3f, vec4f } from 'typegpu/data'
-import { clamp } from 'typegpu/std'
-import { KeyframeTargetProvider, useKeyframeTarget, } from '@/contexts/KeyframeTargetContext'
-import { WheelZoomCamera2D } from '@/lib/WheelZoomCamera2D'
-import { createDragHandler } from '@/utils/createDragHandler'
-import { recordEntries, recordKeys } from '@/utils/record'
-import ui from './App.module.css'
-import { AffineEditor } from './components/AffineEditor/AffineEditor'
-import { Button } from './components/Button/Button'
-import { CollapsibleCard } from './components/CollapsibleCard/CollapsibleCard'
-import { ColorPicker } from './components/ColorPicker/ColorPicker'
-import { Card } from './components/ControlCard/ControlCard'
-import { DebugOverlay } from './components/DebugOverlay'
-import { DiceButton } from './components/DiceButton/DiceButton'
-import { Dropzone } from './components/Dropzone/Dropzone'
+import { createEffect, createResource, createSignal, ErrorBoundary, onCleanup, Show, Suspense, } from 'solid-js'
 import { AppCrashed, WebgpuNotSupported, } from './components/ErrorHandling/ErrorHandling'
-import { createExportPngDialog } from './components/ExportPngDialog/ExportPngDialog'
-import { FlameColorEditor, handleColor, } from './components/FlameColorEditor/FlameColorEditor'
-import { FloatingActions } from './components/FloatingActions/FloatingActions'
-import { createShowHelp } from './components/HelpModal/HelpModal'
-import { createLoadFlame } from './components/LoadFlameModal/LoadFlameModal'
-import { createLogoFaviconGenerator } from './components/LogoFaviconGenerator/LogoFaviconGenerator'
 import { Modal } from './components/Modal/Modal'
-import { PaletteSelector } from './components/PaletteSelector/PaletteSelector'
-import { ProgressBar } from './components/ProgressBar/ProgressBar'
-import { getPresetFromQuality, qualityPresets, } from './components/Quality/QualityPresets'
-import { QuickVariationPicker } from './components/QuickVariationPicker/QuickVariationPicker'
-import { createShareLinkModal } from './components/ShareLinkModal/ShareLinkModal'
-import { Slider } from './components/Sliders/Slider'
-import { SoftwareVersion } from './components/SoftwareVersion/SoftwareVersion'
-import { SpotlightTour } from './components/SpotlightTour/SpotlightTour'
-import { KeyframeDiamond } from './components/Timeline/KeyframeDiamond'
-import { TimelineSection } from './components/Timeline/TimelineSection'
-import { createVariationSelector } from './components/VariationSelector/VariationSelector'
-import { ViewControls } from './components/ViewControls/ViewControls'
 import { WelcomeScreen } from './components/WelcomeScreen/WelcomeScreen'
-import { ChangeHistoryContextProvider } from './contexts/ChangeHistoryContext'
-import { CompactModeProvider, useCompactMode, } from './contexts/CompactModeContext'
+import { CompactModeProvider } from './contexts/CompactModeContext'
+import { KeyframeTargetProvider } from './contexts/KeyframeTargetContext'
 import { createSpotlightTourState, SpotlightTourContext, } from './contexts/SpotlightTourContext'
-import { ThemeContextProvider, useTheme } from './contexts/ThemeContext'
-import { TimelineContextProvider } from './contexts/TimelineContext'
-import { DEFAULT_POINT_COUNT, DEFAULT_QUALITY, DEFAULT_RENDER_INTERVAL_MS, DEFAULT_RESOLUTION, IS_DEV, } from './defaults'
-import { colorInitModeToImplFn } from './flame/colorInitMode'
-import { applyColorMapToFlame } from './flame/colorMap'
-import { drawModeToImplFn } from './flame/drawMode'
-import { example1 } from './flame/examples/example1'
-import { Flam3 } from './flame/Flam3'
-import { pointInitModeToImplFn } from './flame/pointInitMode'
-import { random01, randomizeAllColors, randomizeVariationParams, } from './flame/randomize'
-import { accumulatedPointCount, animationExportCancel, animationExportProgress, animationExportRunning, exportProgress, exportQuality, qualityPointCountLimit, setCurrentQuality, setQualityPointCountLimit, } from './flame/renderStats'
-import { MAX_CAMERA_ZOOM_VALUE, MIN_CAMERA_ZOOM_VALUE, } from './flame/schema/flameSchema'
-import { generateTransformId, generateVariationId, } from './flame/transformFunction'
-import { isParametricVariation, isParametricVariationType, isVariationType, transformVariations, } from './flame/variations'
-import { getNormalizedVariationName, getParamsEditor, getVariationDefault, } from './flame/variations/utils'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Cross, Eye, EyeOff, Menu, Plus, Share } from './icons'
-import { AutoCanvas } from './lib/AutoCanvas'
+import { ThemeContextProvider } from './contexts/ThemeContext'
+import { ToastProvider } from './contexts/ToastContext'
+import { IS_DEV } from './defaults'
 import { Root } from './lib/Root'
+import { MainWorkspace } from './MainWorkspace'
 import { appTour } from './tours/appTour'
 import { sidebarTour } from './tours/sidebarTour'
 import { timelineTour } from './tours/timelineTour'
-import { createAnimationExport } from './utils/animationExport'
-import { deepClone } from './utils/clone'
-import { createStoreHistory } from './utils/createStoreHistory'
 import { decodeSharePayload } from './utils/jsonQueryParam'
 import { persistentSignal } from './utils/persistentSignal'
-import { buildReadableIds } from './utils/readableIds'
-import { saveRecentFlame } from './utils/recentFlames'
-import { sum } from './utils/sum'
-import { createTimelineState, resolveKeyframeValue } from './utils/timeline'
-import { useKeyboardShortcuts } from './utils/useKeyboardShortcuts'
-import { useLoadFlameFromFile } from './utils/useLoadFlameFromFile'
+import { recordKeys } from './utils/record'
 import { dismissWelcome, hasWelcomeBeenDismissed, } from './utils/welcomeDismissed'
-import type { Setter } from 'solid-js'
-import type { v2f } from 'typegpu/data'
-import type { QualityPreset } from './components/Quality/QualityPresets'
-import type { QuickPickerMode } from './components/QuickVariationPicker/QuickVariationPicker'
-import type { TourContext, TourGuide, } from './components/SpotlightTour/tourTypes'
-import type { ColorInitMode } from './flame/colorInitMode'
-import type { ColorMap, Palette } from './flame/colorMap'
-import type { DrawMode } from './flame/drawMode'
-import type { PointInitMode } from './flame/pointInitMode'
-import type { FlameDescriptor, TransformFunction, TransformId, VariationId, } from './flame/schema/flameSchema'
-import type { TransformVariationType } from './flame/variations'
-import type { AnimationExportConfig } from './utils/animationExport'
-import type { SharePayload } from './utils/jsonQueryParam'
-
-const EDGE_FADE_COLOR = {
-  light: vec4f(0.96, 0.96, 0.96, 0.7),
-  dark: vec4f(0, 0, 0, 0.6),
-}
-
-function formatPercent(x: number) {
-  if (x === 1) {
-    return `100 %`
-  }
-  return `${(x * 100).toFixed(1)} %`
-}
+import type { TourGuide } from './components/SpotlightTour/tourTypes'
+import type { FlameDescriptor } from './flame/schema/flameSchema'
 
 function getTour(id: string): TourGuide | undefined {
   switch (id) {
@@ -113,43 +33,21 @@ function getTour(id: string): TourGuide | undefined {
   }
 }
 
-function newDefaultTransform(): TransformFunction {
-  return {
-    probability: 1,
-    color: { x: 0, y: 0 },
-    preAffine: { a: 1, b: 0, c: 0, d: 0, e: 1, f: 0 },
-    postAffine: { a: 1, b: 0, c: 0, d: 0, e: 1, f: 0 },
-    visible: true,
-    variations: {
-      [generateVariationId()]: getVariationDefault('linear', 1.0),
-    },
-  }
-}
-
 export type ExportImageType = (canvas: HTMLCanvasElement) => void
 
-type AppProps = {
-  flameFromQuery?: SharePayload
-  flameFromWelcome?: () => FlameDescriptor | undefined
-  resetFlameFromWelcome?: () => void
-}
-
-
-import { MainWorkspace } from "./MainWorkspace"
-import { ToastProvider } from "./contexts/ToastContext"
-
 export function Wrappers() {
+  const [showWelcome, setShowWelcome] = createSignal(!hasWelcomeBeenDismissed())
+  const [dontShowAgain, setDontShowAgain] = persistentSignal(
+    'dontShowWelcome',
+    false,
+  )
+  const [selectedFlame, setSelectedFlame] = createSignal<
+    FlameDescriptor | undefined
+  >()
+
   const [flameFromQuery] = createResource(async () => {
-    const param = new URLSearchParams(window.location.search)
-    const flameDef = param.get('flame')
-    if (IS_DEV) {
-      console.info(
-        '[share:resource] query param present:',
-        flameDef !== null,
-        'length:',
-        flameDef?.length ?? 0,
-      )
-    }
+    const urlParams = new URLSearchParams(window.location.search)
+    const flameDef = urlParams.get('q')
     if (flameDef !== null) {
       try {
         const result = await decodeSharePayload(flameDef)
@@ -164,34 +62,24 @@ export function Wrappers() {
           })
         }
         return result
-      } catch (ex) {
-        console.error('[share:resource] decode failed:', ex)
+      } catch (err) {
+        console.error('Failed to decode share payload:', err)
       }
     }
     return undefined
   })
 
-  const [dontShowAgain, setDontShowAgain] = createSignal(false)
-  const hasFlameQueryParam = new URLSearchParams(window.location.search).has(
-    'flame',
-  )
-  const [showWelcome, setShowWelcome] = createSignal(
-    !hasWelcomeBeenDismissed() && !hasFlameQueryParam,
-  )
-  const [selectedFlame, setSelectedFlame] = createSignal<
-    FlameDescriptor | undefined
-  >()
-
   const spotlightState = createSpotlightTourState(getTour)
 
   // Support #tour=app|sidebar|timeline hash URLs
-  onMount(() => {
+  createEffect(() => {
     const handleHashChange = () => {
       const hash = window.location.hash
-      const match = hash.match(/^#tour=(app|sidebar|timeline)$/)
+      const match = /#tour=([a-zA-Z0-9_-]+)/.exec(hash)
       if (match) {
-        spotlightState.startTour(match[1]!)
-        window.location.hash = ''
+        const tourId = match[1]!
+        setShowWelcome(false)
+        spotlightState.startTour(tourId)
       }
     }
 
