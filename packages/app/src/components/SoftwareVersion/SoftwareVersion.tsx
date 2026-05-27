90eed0c291fd764153904937ac406e65bb76c4ed
komediruzecki <komediruzecki.2015@gmail.com>
2026-05-27 16:09:35 +0200

    Improve version info about panel

--- packages/app/src/components/SoftwareVersion/SoftwareVersion.tsx
+++ packages/app/src/components/SoftwareVersion/SoftwareVersion.tsx
@@ -1,14 +1,25 @@
 import { VERSION } from '@/version'
 import { DebugPanel } from '../Debug/DebugPanel'
-import { QuestionMark } from '../QuestionMark/QuestionMark'
 import ui from './SoftwareVersion.module.css'
 
 export function SoftwareVersion(props: { showHelp: () => void }) {
   return (
     <div>
       <DebugPanel />
-      <button class={ui.version} onClick={props.showHelp}>
-        CM v{VERSION} <QuestionMark />
+      <button class={ui.aboutPill} onClick={props.showHelp}>
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="10" />
+          <line x1="12" y1="16" x2="12" y2="12" />
+          <line x1="12" y1="8" x2="12.01" y2="8" />
+        </svg>
+        v{VERSION}
       </button>
     </div>
   )
