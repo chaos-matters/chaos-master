6802650f30ac46baf07b5ffad53723666f92c3f7
komediruzecki <komediruzecki.2015@gmail.com>
2026-08-13 23:36:29 +0200

    chore(deps): upgrade TypeGPU ecosystem to 0.12

--- packages/app/src/flame/variations/custom/runtimeCompiler.ts
+++ packages/app/src/flame/variations/custom/runtimeCompiler.ts
@@ -315,7 +315,8 @@ export function compileCustomVariationCode(wgslBody: string): CompileResult {
     }
   }
 
-  const { params, body, externalNames } = irResult
+  const { params, body } = irResult
+  const externalNames = [...irResult.externalNames.keys()]
 
   const banned = externalNames.filter((name: string) => BANNED_NAMES.has(name))
   if (banned.length > 0) {
